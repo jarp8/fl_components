d7d2af8b430acaf841ab02e0f1b5542a6dc0aba7
RetroMac <retromac@MacBook-Air-de-RetroMac.local>
2022-06-07 15:08:11 -0500

    Widgets con argumentos

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,11 +16,9 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(imageUrl: 'https://www.dogalize.com/wp-content/uploads/2017/06/La-sverminazione-e-la-pulizia-del-cucciolo-del-cane-2-800x400-800x400.jpg', name: 'Camada de perritos',),
           SizedBox(height: 10),
-          CustomCardType2(),
-          SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(imageUrl: 'https://images.theconversation.com/files/125391/original/image-20160606-13080-s7o3qu.jpg?ixlib=rb-1.1.0&rect=273%2C0%2C2639%2C1379&q=45&auto=format&w=926&fit=clip',),
         ],
       ),
     );
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,11 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+
+  final String imageUrl;
+  final String? name;
+  
+  const CustomCardType2({Key? key, required this.imageUrl, this.name}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +17,20 @@ class CustomCardType2 extends StatelessWidget {
       shadowColor: AppTheme.primary,
       child: Column(
         children: [
-          const FadeInImage(
+          FadeInImage(
             image: NetworkImage(
-                'https://www.dogalize.com/wp-content/uploads/2017/06/La-sverminazione-e-la-pulizia-del-cucciolo-del-cane-2-800x400-800x400.jpg'),
-            placeholder: AssetImage('assets/jar-loading.gif'),
+                imageUrl),
+            placeholder: const AssetImage('assets/jar-loading.gif'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
+          if(name != null)
           Container(
             alignment: AlignmentDirectional.centerEnd,
             padding: const EdgeInsets.only(right: 20, top: 20, bottom: 20),
-            child: Text('Hermosa camada'),
+            child: Text(name!),
           )
         ],
       ),
